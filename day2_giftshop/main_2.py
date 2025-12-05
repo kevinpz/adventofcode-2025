ebb021635a6710e34d18aad15645d9a627a05a6d
Kevin Printz <kevinprintz@microsoft.com>
2025-12-05 13:32:30 -0800

    completed part2 day2

--- day2_giftshop/main_2.py
+++ day2_giftshop/main_2.py
@@ -1,29 +1,29 @@
 def main():
-    pos = 50
-    code = 0
+    invalid_ids = 0
+
+    # Read input
     with open("input.txt", "r") as f:
-        for line in f:
-            direction = line[0]
-            steps = int(line[1:])
+        input=f.read()
 
-            if direction == "L":
-                steps *= -1
+    # Split input
+    ranges = input.split(",")
+    for r in ranges:
+        start, end = map(int, r.split("-")) # Get start and end in int
 
-            tmp_pos = pos + steps
-            
-            # count if we pass 0 from going between positive and negative
-            if 0 in range(min(pos, tmp_pos)+1, max(pos, tmp_pos)): 
-                code += 1
+        # Iterate over range
+        for i in range(start, end + 1):
+            nb = str(i)
 
-            pos = tmp_pos % 100
-            # if we have a big rotation count how many times we pass 0
-            if abs(tmp_pos) >= 100:
-                code += abs(tmp_pos) // 100
-            # if it's a small rotation and we end up at 0 count it
-            elif pos == 0:
-                code += 1
+            # Iterate over all length for current number
+            for j in range(1,len(nb)):
+                nb_split = [nb[k:k+j] for k in range(0, len(nb), j)]
+                nb_unique = set(nb_split)
+                if len(nb_unique) == 1:
+                    invalid_ids += i
+                    break
 
-    print(code)
+    
+    print(invalid_ids)
 
 
 if __name__ == "__main__":  
